7e9a45831082a422a54debd508152b734baca99d
Bryan Dunk <idrumgood@gmail.com>
2025-11-19 18:39:30 -0600

    make sure page scrolls appropriately on load

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Intro from '../components/Intro';
 import About from './About';
 import Interests from './Interests';
 import Contact from './Contact';
 
 const Home = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.hash) {
+            const element = document.querySelector(location.hash);
+            if (element) {
+                setTimeout(() => {
+                    element.scrollIntoView({ behavior: 'smooth' });
+                }, 100); // Small delay to ensure DOM is ready
+            }
+        }
+    }, [location]);
+
     return (
         <>
             <Intro />
